acfc4e55e514448c75c8666ef17d0493bb6c1dcc
Painkiller995 <fahdakramdaher@gmail.com>
2025-03-11 07:52:19 +0100

    update selection sort implementation

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -19,17 +19,14 @@ def sort(arr: list[int]) -> None:
     """
     number_of_elements = len(arr)
 
-    # Assuming the array is already sorted.
-    is_sorted = True
-
     for i in range(number_of_elements):
-        for j in range(number_of_elements):
-            if arr[j] > arr[i]:
-                arr[i], arr[j] = arr[j], arr[i]
-                is_sorted = False
+        min_index = i
 
-        if is_sorted:  # If the array is already sorted, break the loop.
-            break
+        for j in range(i + 1, number_of_elements):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
 
 if __name__ == "__main__":
