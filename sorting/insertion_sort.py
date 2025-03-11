b667f679ba6ab04e7d69b652da90c64419494b2d
Painkiller995 <fahdakramdaher@gmail.com>
2025-03-11 09:46:16 +0100

    rename the element

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -20,13 +20,13 @@ def sort(arr: list[int]) -> None:
     number_of_elements = len(arr)
 
     for i in range(1, number_of_elements):  # start from the second element
-        key = arr[i]
+        current_element = arr[i]
         j = i - 1
         # Move elements of arr[0..i-1], that are greater than key, to one position ahead
-        while j >= 0 and key < arr[j]:
+        while j >= 0 and current_element < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = key
+        arr[j + 1] = current_element
 
 
 if __name__ == "__main__":
