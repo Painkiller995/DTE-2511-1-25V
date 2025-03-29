d7c3a66dc2509de128557b5c815c284cbf38e8cb
Painkiller995 <fahdakramdaher@gmail.com>
2025-03-29 09:00:06 +0100

    update eval postfix to follow the task

--- eval_postfix.py
+++ eval_postfix.py
@@ -5,6 +5,10 @@ This implementation could be improved in the feature please check github for the
 https://github.com/Painkiller995/DTE-2511-1-25V
 """
 
+# Using the infix_to_postfix provided in the task description
+# Please remove the print statements in the infix_to_postfix function before using it
+from infix_to_postfix_start import infix_to_postfix
+
 
 def evaluate_postfix(expression: str) -> int:
     """
@@ -17,7 +21,7 @@ def evaluate_postfix(expression: str) -> int:
         int: The result of the expression
     """
     stack = []
-    operators = {"+", "-", "*", "/", "^"}
+    operators = {"+", "-", "*", "/", "%", "^"}
 
     for token in expression.split():
         if token.isdigit():  # If token is a number, push it on the stack
@@ -44,6 +48,11 @@ def evaluate_postfix(expression: str) -> int:
                     raise ZeroDivisionError("Division by zero")
                 stack.append(a // b)  # Integer division
 
+            elif token == "%":
+                if b == 0:
+                    raise ZeroDivisionError("Division by zero")
+                stack.append(a % b)
+
             elif token == "^":
                 stack.append(a**b)
 
@@ -60,10 +69,13 @@ def main() -> None:
     """
     Main function for the postfix expression evaluation program.
     """
-    postfix_expression = input("Enter a postfix expression: ")
+    expressions = ["(1 + 2) * 3", " 2 * 3 - 4 + 8 * 2", "(6-9)*(3+4)^2"]
     try:
-        result = evaluate_postfix(postfix_expression)
-        print("Result:", result)
+        for expression in expressions:
+            postfix_expression = infix_to_postfix(expression)  # type: ignore
+            print("Infix expression:", expression, "Postfix expression:", postfix_expression)
+            result = evaluate_postfix(postfix_expression)
+            print("Result:", result)
     except Exception as e:
         print("Error:", e)
 
