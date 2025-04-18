42d5beeb3275d09c1aa44ad645a7b5f3b8d5946d
Painkiller995 <fahdakramdaher@gmail.com>
2025-04-18 12:17:11 +0200

    add course bst

--- bst.py
+++ bst.py
@@ -1,91 +1,177 @@
-"""
-This module is an implementation of a binary search tree.
+class BST:
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
-This implementation could be improved in the feature please check github for the latest version.
-https://github.com/Painkiller995/DTE-2511-1-25V
-"""
+    # Return True if the element is in the tree
+    def search(self, e):
+        current = self.root  # Start from the root
 
-from typing import Optional
+        while current != None:
+            if e < current.element:
+                current = current.left
+            elif e > current.element:
+                current = current.right
+            else:  # element matches current.element
+                return True  # Element is found
 
+        return False
 
-class BinarySearchTree:
-    """
-    A binary search tree (BST) implementation.
-    """
-
-    def __init__(self) -> None:
-        self._root: Node | None = None
-
-    def insert(self, value: int) -> None:
-        """
-        Insert a value into the BST.
-        """
-        self._root = self._insert_recursive(self._root, value)
-
-    def _insert_recursive(self, node: Optional["Node"], value: int) -> "Node":
-        if node is None:
-            return Node(value)
-
-        if node.element and value < node.element:
-            node.left = self._insert_recursive(node.left, value)
+    # Insert element e into the binary search tree
+    # Return True if the element is inserted successfully
+    def insert(self, e):
+        if self.root == None:
+            self.root = self.createNewNode(e)  # Create a new root
         else:
-            node.right = self._insert_recursive(node.right, value)
+            # Locate the parent node
+            parent = None
+            current = self.root
+            while current != None:
+                if e < current.element:
+                    parent = current
+                    current = current.left
+                elif e > current.element:
+                    parent = current
+                    current = current.right
+                else:
+                    return False  # Duplicate node not inserted
 
-        return node
+            # Create the new node and attach it to the parent node
+            if e < parent.element:
+                parent.left = self.createNewNode(e)
+            else:
+                parent.right = self.createNewNode(e)
+
+        self.size += 1  # Increase tree size
+        return True  # Element inserted
+
+    # Create a new TreeNode for element e
+    def createNewNode(self, e):
+        return TreeNode(e)
+
+    # Return the size of the tree
+    def getSize(self):
+        return self.size
+
+    # Inorder traversal from the root
+    def inorder(self):
+        self.inorderHelper(self.root)
+
+    # Inorder traversal from a subtree
+    def inorderHelper(self, r):
+        if r != None:
+            self.inorderHelper(r.left)
+            print(r.element, end=" ")
+            self.inorderHelper(r.right)
+
+    # Postorder traversal from the root
+    def postorder(self):
+        self.postorderHelper(self.root)
+
+    # Postorder traversal from a subtree
+    def postorderHelper(self, root):
+        if root != None:
+            self.postorderHelper(root.left)
+            self.postorderHelper(root.right)
+            print(root.element, end=" ")
+
+    # Preorder traversal from the root
+    def preorder(self):
+        self.preorderHelper(self.root)
+
+    # Preorder traversal from a subtree
+    def preorderHelper(self, root):
+        if root != None:
+            print(root.element, end=" ")
+            self.preorderHelper(root.left)
+            self.preorderHelper(root.right)
+
+    # Returns a path from the root leading to the specified element
+    def path(self, e):
+        list = []
+        current = self.root  # Start from the root
+
+        while current != None:
+            list.append(current)  # Add the node to the list
+            if e < current.element:
+                current = current.left
+            elif e > current.element:
+                current = current.right
+            else:
+                break
+
+        return list  # Return an array of nodes
+
+    # Delete an element from the binary search tree.
+    # Return True if the element is deleted successfully
+    # Return False if the element is not in the tree
+    def delete(self, e):
+        # Locate the node to be deleted and its parent node
+        parent = None
+        current = self.root
+        while current != None:
+            if e < current.element:
+                parent = current
+                current = current.left
+            elif e > current.element:
+                parent = current
+                current = current.right
+            else:
+                break  # Element is in the tree pointed by current
+
+        if current == None:
+            return False  # Element is not in the tree
+
+        # Case 1: current has no left children
+        if current.left == None:
+            # Connect the parent with the right child of the current node
+            if parent == None:
+                self.root = current.right
+            else:
+                if e < parent.element:
+                    parent.left = current.right
+                else:
+                    parent.right = current.right
+        else:
+            # Case 2: The current node has a left child
+            # Locate the rightmost node in the left subtree of
+            # the current node and also its parent
+            parentOfRightMost = current
+            rightMost = current.left
+
+            while rightMost.right != None:
+                parentOfRightMost = rightMost
+                rightMost = rightMost.right  # Keep going to the right
+
+            # Replace the element in current by the element in rightMost
+            current.element = rightMost.element
+
+            # Eliminate rightmost node
+            if parentOfRightMost.right == rightMost:
+                parentOfRightMost.right = rightMost.left
+            else:
+                # Special case: parentOfRightMost == current
+                parentOfRightMost.left = rightMost.left
+
+        self.size -= 1
+        return True  # Element deleted
+
+    # Return true if the tree is empty
+    def isEmpty(self):
+        return self.size == 0
+
+    # Remove all elements from the tree
+    def clear(self):
+        self.root == None
+        self.size == 0
+
+    # Return the root of the tree
+    def getRoot(self):
+        return self.root
 
 
-class Node:
-    """
-    A node in a binary search tree.
-    """
-
-    def __init__(self, element: int | None = None) -> None:
-        self._element: int | None = element
-        self._left: Node | None = None
-        self._right: Node | None = None
-
-    @property
-    def element(self) -> int | None:
-        """
-        The element stored in this node.
-        """
-        return self._element
-
-    @element.setter
-    def element(self, value: int) -> None:
-        self._element = value
-
-    @property
-    def left(self) -> Optional["Node"]:
-        """
-        The left child of this node.
-        """
-        return self._left
-
-    @left.setter
-    def left(self, value: Optional["Node"]) -> None:
-        self._left = value
-
-    @property
-    def right(self) -> Optional["Node"]:
-        """
-        The right child of this node.
-        """
-        return self._right
-
-    @right.setter
-    def right(self, value: Optional["Node"]) -> None:
-        self._right = value
-
-    def __str__(self) -> str:
-        return f"Node = {self._element}"
-
-
-if __name__ == "__main__":
-    print("Hello")
-
-    bst = BinarySearchTree()
-    bst.insert(1)
-    bst.insert(2)
-
-    print(bst._root)
+class TreeNode:
+    def __init__(self, e):
+        self.element = e
+        self.left = None  # Point to the left node, default None
+        self.right = None  # Point to the right node, default None
