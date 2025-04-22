fb485941d13fd4756a9f1f9c3ea7233013a6f1d2
Painkiller995 <fahdakramdaher@gmail.com>
2025-04-22 14:44:43 +0200

    add topological_sort

--- graph_topological_sorting.py
+++ graph_topological_sorting.py
@@ -10,6 +10,35 @@ class GraphTopologicalSorting(Graph):
     A class for representing a graph and performing topological sorting.
     """
 
+    def __init__(self):
+        super().__init__()
+        self._stack = []
+        self._visited = set()
+
+    def topological_sort(self, label: str) -> list:
+        """
+        Performs topological sorting on the graph.
+        """
+        if label not in self._nodes:
+            raise ValueError("Node not found in the graph.")
+
+        node = self._nodes[label]
+        self._topological_sort_util(node)
+
+        return self._stack[::-1]  # Return in reverse order
+
+    def _topological_sort_util(self, node) -> None:
+        """
+        A utility function to perform topological sorting.
+        """
+        self._visited.add(node)
+
+        for neighbor in self._adjacency_list[node]:
+            if neighbor not in self._visited:
+                self._topological_sort_util(neighbor)
+
+        self._stack.append(node)
+
 
 if __name__ == "__main__":
     graph = GraphTopologicalSorting()
@@ -23,3 +52,8 @@ if __name__ == "__main__":
     graph.add_edge("B", "P")
     print(graph)
     print("--" * 20)
+
+    print("Topological Sort:")
+    sorted_nodes = graph.topological_sort("X")
+    for node in sorted_nodes:
+        print(node._label, end=" ")
